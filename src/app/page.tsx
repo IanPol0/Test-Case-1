66c2b8c82419ebfdced8e0f105af9af5f4d306e3
IanPol0 <ipolonuer@gmail.com>
2024-07-24 17:54:20 -0300

    fix (page.tsx): table rows were not in table body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const data = await getData();
   const keys = Object.keys(data[0]);
 
-  const handleChange = (event)=>{
+  const handleChange = (event:Event)=>{
     console.log(event)
   }
 
@@ -23,18 +23,20 @@ export default async function Home() {
               ))}
             </tr>
           </thead>
-          {data.map((user, index)=>(
-          <tr key={index} className="text-center">
-            <td className="border px-5 py-2">{user.id}</td>
-            <td className="border px-5 py-2">{user.name}</td>
-            <td className="border px-5 py-2">{user.username}</td>
-            <td className="border px-5 py-2">{user.email}</td>
-            <td className="border px-5 py-2">adress</td>
-            <td className="border px-5 py-2">{user.phone}</td>
-            <td className="border px-5 py-2">{user.website}</td>
-            <td className="border px-5 py-2">company</td>
-          </tr>
-        ))}
+          <tbody>
+            {data.map((user:any, index:any)=>(
+            <tr key={index} className="text-center">
+              <td className="border px-5 py-2">{user.id}</td>
+              <td className="border px-5 py-2">{user.name}</td>
+              <td className="border px-5 py-2">{user.username}</td>
+              <td className="border px-5 py-2">{user.email}</td>
+              <td className="border px-5 py-2">adress</td>
+              <td className="border px-5 py-2">{user.phone}</td>
+              <td className="border px-5 py-2">{user.website}</td>
+              <td className="border px-5 py-2">company</td>
+            </tr>
+            ))}
+          </tbody>
         </table>
         
       </section>
